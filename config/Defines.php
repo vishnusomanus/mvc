5d6780e21363bb385252001251a0e0e9770a2295
Vishnu Soman U S <vishnusomanus@gmail.com>
2018-08-11 12:34:26 +0530

    Add files via upload

---
+++ config/Defines.php
@@ -0,0 +1,25 @@
+<?php
+define('APP_PATH', ROOT_PATH. '/app/');
+define('CONFIG_PATH', APP_PATH. '/config/');
+define('VENDOR_PATH', ROOT_PATH. '/vendor/');
+define('VIEW_PATH', ROOT_PATH. '/view/');
+define('CONTROLLER_PATH', ROOT_PATH. '/controller/');
+define('MODEL_PATH', ROOT_PATH. '/model/');
+define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST']);
+define('ASSET', $_SERVER['HTTP_HOST'].'/assets/');
+define('UPLOAD_URL', $_SERVER['HTTP_HOST'].'/assets/uploads/user');
+
+
+
+/*
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'dicon');
+define('DB_USER', 'root');
+define('DB_PASS', 'admin');*/
+
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'akshaya');
+define('DB_USER', 'root');
+define('DB_PASS', '');
